18998fd6b1a58126bb439b6b1b807ccc95852261
butter9fe <hlilian1468@gmail.com>
2025-08-06 23:08:28 +0800

    Update web_library.py

--- web_library.py
+++ web_library.py
@@ -5,12 +5,8 @@ df: pd.DataFrame = pd.read_csv(FILE_NAME)
 
 def get_country_variable(country: str, variable: str = "LCR"):
     country_row = df.loc[df['Country'] == country]
-    return country_row[variable]
+    return float(country_row[variable])
 
-def get_variable_mean(variable: str):
+def get_variable_median(variable: str):
     row = df[variable]
-    return row.mean()
-
-print(get_country_variable("Myanmar"))
-print(get_country_variable("Myanmar", "AQI"))
-print(get_variable_mean("GDP"))
+    return row.median()
