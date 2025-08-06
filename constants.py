7850efc46aa14abd4c90db479b63251eae47772b
Joonhyung Song / Charlie <sjh.chr1lie@gmail.com>
2025-08-06 14:32:41 +0800

    Update constants.py
    added country list

--- constants.py
+++ constants.py
@@ -1,2 +1,24 @@
 ALL_FEATURES = ["HDI", "SR", "EPI", "AQI", "PWD_A", "OADR", "GDP", "AST", "OOP"]
 TARGET = "LCR_OR"
+countries_list = [
+    "Albania", "Algeria", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan",
+    "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Benin", "Bolivia",
+    "Bosnia and Herzegovina", "Botswana", "Brazil", "Bulgaria", "Burkina Faso", "Burundi",
+    "Cambodia", "Cameroon", "Canada", "Chad", "Chile", "China", "Colombia", "Comoros",
+    "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark",
+    "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Estonia", "Ethiopia",
+    "Finland", "France", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Guatemala",
+    "Guinea-Bissau", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia",
+    "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
+    "Kazakhstan", "Kenya", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho",
+    "Liberia", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Mali",
+    "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Mongolia", "Montenegro",
+    "Morocco", "Myanmar", "Namibia", "Nepal", "Netherlands", "New Zealand", "Nigeria",
+    "Norway", "Oman", "Pakistan", "Panama", "Paraguay", "Peru", "Philippines", "Poland",
+    "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saudi Arabia", "Senegal",
+    "Serbia", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa",
+    "South Korea", "Spain", "Sri Lanka", "Sweden", "Switzerland", "Tanzania", "Thailand",
+    "Togo", "Tunisia", "Turkey", "Turkmenistan", "Uganda", "Ukraine",
+    "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan",
+    "Vietnam", "Zambia", "Zimbabwe"
+]
